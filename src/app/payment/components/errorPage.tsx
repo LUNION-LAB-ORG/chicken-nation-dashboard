2ccfe50b4cca125d368a66eb746876e7fe04a723
Anderson Kouadio <andersonkouadio0118@gmail.com>
2025-05-14 12:12:52 +0000

    mise a jour

--- src/app/payment/components/errorPage.tsx
+++ src/app/payment/components/errorPage.tsx
@@ -1,14 +1,14 @@
 const ErrorPage = () => {
-  return (
-    <div className="min-h-screen flex items-center justify-center bg-white px-4">
-      <div className="text-center">
-        <h1 className="text-2xl font-bold mb-2 text-[#F17922] ">Erreur : données manquantes</h1>
-        <p className="text-gray-600 mb-6">
-          Veuillez vérifier les données de paiement.
-        </p>
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-white px-4">
+        <div className="text-center">
+          <h1 className="text-2xl font-bold mb-2 text-[#F17922] ">Erreur : données manquantes</h1>
+          <p className="text-gray-600 mb-6">
+            Veuillez vérifier les données de paiement.
+          </p>
+        </div>
       </div>
-    </div>
-  );
-};
-
-export default ErrorPage;
+    );
+  };
+  
+  export default ErrorPage;
